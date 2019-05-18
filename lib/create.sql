81b9f793d84e114f810fada9455ad99b6e3a7d2b
Ryan Pethel <arpethel@gmail.com>
2019-05-18 18:14:34 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,6 @@
-CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE projects
+(
+    id INTEGER PRIMARY KEY,
     title TEXT,
     category INTEGER,
     funding_goal INTEGER,
@@ -7,14 +8,16 @@ CREATE TABLE projects (
     end_date TEXT
 );
 
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER
 );
 
-CREATE TABLE pledges (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE pledges
+(
+    id INTEGER PRIMARY KEY,
     amount INTEGER,
     user_id INTEGER,
     project_id INTEGER
